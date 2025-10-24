88c5d9be4f532471d2912c66c701cbbe077028e7
AlejandroAlv12 <00211623@uca.edu.sv>
2025-10-24 16:31:35 -0600

    feat: agregar cuentas

---
+++ cuentas-BE/data/cuentas.js
@@ -0,0 +1,68 @@
+const cuentas = [
+  {
+    _id: "68f863f0196d296c9e219f27",
+    isActive: true,
+    picture: "https://cdn-icons-png.flaticon.com/512/4139/4139951.png",
+    balance: "252523",
+    client: "Katie Woodard",
+    gender: "female",
+  },
+  {
+    _id: "68f863f0196d296c9e219f28",
+    isActive: false,
+    picture: "https://cdn-icons-png.flaticon.com/512/4139/4139981.png",
+    balance: "1005000",
+    client: "Alejandro Alvarenga",
+    gender: "male",
+  },
+  {
+    _id: "68f863f0196d296c9e219f29",
+    isActive: true,
+    picture: "https://cdn-icons-png.flaticon.com/512/4139/4139951.png",
+    balance: "56000",
+    client: "Daniela Gómez",
+    gender: "female",
+  },
+  {
+    _id: "68f863f0196d296c9e219f30",
+    isActive: false,
+    picture: "https://cdn-icons-png.flaticon.com/512/4139/4139981.png",
+    balance: "67500",
+    client: "Luis Fernandez",
+    gender: "male",
+  },
+  {
+    _id: "68f863f0196d296c9e219f31",
+    isActive: true,
+    picture: "https://cdn-icons-png.flaticon.com/512/4139/4139981.png",
+    balance: "205423",
+    client: "Enrique Portillo",
+    gender: "male",
+  },
+  {
+    _id: "68f863f0196d296c9e219f32",
+    isActive: false,
+    picture: "https://cdn-icons-png.flaticon.com/512/4139/4139951.png",
+    balance: "2",
+    client: "Lucia Guerra",
+    gender: "female",
+  },
+  {
+    _id: "68f863f0196d296c9e219f33",
+    isActive: true,
+    picture: "https://cdn-icons-png.flaticon.com/512/4139/4139951.png",
+    balance: "10000",
+    client: "Martha Martinez",
+    gender: "female",
+  },
+  {
+    _id: "68f863f0196d296c9e219f34",
+    isActive: true,
+    picture: "https://cdn-icons-png.flaticon.com/512/4139/4139981.png",
+    balance: "9999999",
+    client: "Emanuel Araujo",
+    gender: "male",
+  }
+];
+
+module.exports = cuentas;
